ed307a8090320bb49c52c6476e3e810a8e25a6a5
julien <juliencauwet@yahoo.fr>
2019-01-21 20:53:40 +0100

    changed apllicationContext pATH

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertNotNull;
  * Classe de test de l'initialisation du contexte Spring
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:com/dummy/myerp/business/applicationContext.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class TestInitSpring extends BusinessTestCase {
 
     /**
